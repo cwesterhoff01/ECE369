0b966e39a561de93b80e3885b00a153ae41757ee
cwesterhoff01 <93213402+cwesterhoff01@users.noreply.github.com>
2021-11-22 14:49:46 -0700

    Adding in comments to do

--- Project/TopLevel.v
+++ Project/TopLevel.v
@@ -77,21 +77,28 @@ module TopLevel(Clk,Rst,PCCheck,WriteDataCheck,HICheck,LOCheck);
     ProgramCounter PC(WritePC,PCInput,PCOutput,Clk,Rst);
     Adder PCAdd(PCOutput,32'd4,PCAddResult);
     InstructionMemory IM(PCOutput,Instruction,Rst);
+    //change pipeline registers
     RegisterIFID IFID(WriteIFID,branch,{Instruction,PCAddResult},IFIDOut,Clk,Rst);
-  //HazardDetectionUnit HDU(IFIDRs,        IFIDRt,       EXRd,        EXMEMRd,        IFIDRegWrite,  IFIDRegDst,    IDEXRegWrite,EXMEMRegWrite,MEMWBRd,    MEMWBRegWrite,PCWrite,IFIDWrite,ControlWrite);
-    HazardDetectionUnit HDU(IFIDOut[57:53],IFIDOut[52:48],RdMuxOutput,EXMEMOut[74:70],ControlBits[0],ControlBits[6],IDEXOut[0],EXMEMOut[0],MEMWBOut[70:66],MEMWBOut[0],WritePC,WriteIFID,WriteControl);
+  //HazardDetectionUnit HDU(IFIDRs,        IFIDRt,       EXRd,        EXMEMRd,        IFIDRegDst,    IDEXRegWrite,EXMEMRegWrite,MEMWBRd,    MEMWBRegWrite,PCWrite,IFIDWrite,ControlWrite);
+    HazardDetectionUnit HDU(IFIDOut[57:53],IFIDOut[52:48],RdMuxOutput,EXMEMOut[74:70],ControlBits[6],IDEXOut[0],EXMEMOut[0],MEMWBOut[70:66],MEMWBOut[0],WritePC,WriteIFID,WriteControl);
     Controller control(IFIDOut[63:32],equalVal,gtZero,ltZero,beqz,ControlBits,Jump,PCSrc, branch);
     Comparator comp(RegData1,RegData2,equalVal,gtZero,ltZero,beqz);
+    //add sign extender
     SignExtender SE(IFIDOut[47:32],Immediate);
+    //change regFil
     RegisterFile RegFile(IFIDOut[57:53],IFIDOut[52:48],JALMux2Output,DataMuxOutput,MEMWBOut[0],RegData1,RegData2,Clk,Rst);
     Mux13Bit2To1 controlMux(13'b0,ControlBits,WriteControl,ControlMuxOutput);
     LeftShifter LS(Immediate,BranchOffset);
     Adder adder(BranchOffset,IFIDOut[31:0],BranchAddress);
+    //change pipeline register
     RegisterIDEX IDEX(1'b1,{IFIDOut[31:0],Jump,IFIDOut[42:38],IFIDOut[47:43],IFIDOut[52:48],IFIDOut[57:53],Immediate,RegData2,RegData1,ControlMuxOutput},IDEXOut,Clk,Rst);
     Mux5Bit2To1 RdMux(IDEXOut[118:114],IDEXOut[123:119],IDEXOut[6],RdMuxOutput);
     Mux32Bit2To1 ALUMux3(IDEXOut[76:45],IDEXOut[108:77],IDEXOut[12],ALUMux3Output);
+    //add new ALU & change ALU outputs
     ALU alu(IDEXOut[44:13],ALUMux3Output,IDEXOut[11:7],IDEXOut[128:124],HIOutput,LOOutput,ALUResult,Zero,Clk,Rst);
+    //Change pipeline register
     RegisterEXMEM EXMEM(1'b1,{IDEXOut[162:131],IDEXOut[130:129],RdMuxOutput,IDEXOut[76:45],ALUResult,IDEXOut[5:0]},EXMEMOut,Clk,Rst);
+    //Change datamemory inputs
     DataMemory DM(EXMEMOut[3],EXMEMOut[37:6],EXMEMOut[69:38],EXMEMOut[2],EXMEMOut[5:4],DataOutput,Clk,Rst);
     RegisterMEMWB MEMWB(1'b1,{EXMEMOut[108:77],EXMEMOut[76:75],EXMEMOut[74:70],EXMEMOut[37:6],DataOutput,EXMEMOut[1:0]},MEMWBOut,Clk,Rst);
     Mux32Bit2To1 JALMux1(MEMWBOut[65:34],MEMWBOut[104:73],MEMWBOut[72]&MEMWBOut[71],JALMux1Output);
